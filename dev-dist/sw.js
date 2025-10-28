feed687b87e4075284945bdfd2429b3e508dcb18
nishal21 <nishalamv@gmail.com>
2025-10-28 18:42:00 +0530

    Fix missing CommentSection import in Watch.jsx
    - Add import for CommentSection component to resolve ReferenceError
    - Fixes runtime error preventing comments from displaying on watch pages

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-fc76d6c3'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.6i7a5j8b4og"
+    "revision": "0.ltkn7tgj8j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
